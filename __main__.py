505412ea779f36606596e1d186ce332b5e658b41
Gwangmu Lee <gwangmu@epfl.ch>
2024-08-29 21:52:18 +0200

    ㅍ, ㅂ annotation placement fine tuning

--- __main__.py
+++ __main__.py
@@ -50,14 +50,13 @@ if (not args.no_draw and args.draw_output):
         else:
             os.mkdir(_dirname)
 
-'''
 print("test start")
 
 import hclasses as hcl
 import tables as tbl
 
 test_vowel = ['ㅏ', 'ㅓ', 'ㅔ', 'ㅗ', 'ㅜ', 'ㅡ', 'ㅣ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅝ', 'ㅞ', 'ㅢ']
-test_con = [['ㅂ', 'ㅍ'], ['ㅈ'], ['ㄹ', 'ㄷ', 'ㄸ']]
+test_con = [['ㅈ'], ['ㄹ', 'ㄷ', 'ㄸ'], ['ㅂ', 'ㅍ']]
 
 for con_group in test_con:
     test_hcl_sent = []
@@ -69,7 +68,6 @@ for con_group in test_con:
     api.draw(test_hcl_sent)
 
 print("test end")
-'''
 
 # Set up the arguments to pass
 pass_args = {
